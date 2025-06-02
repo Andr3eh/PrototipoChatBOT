0d85d994fc0a2e96a71a79297dfac7d0277de3da
Andr3eh <andre.eugenio77@hotmail.com>
2025-06-02 11:54:47 -0300

    Criando função para não repetir usuarios

--- clientes.py
+++ clientes.py
@@ -4,6 +4,7 @@
 
 import csv
 import os
+import time
 
 
 def verificar_dados(nome, cpf, telefone, email):
@@ -27,37 +28,56 @@ def verificar_dados(nome, cpf, telefone, email):
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def cpf_ja_cadastrado(cpf):
+    if not os.path.exists('clientes.csv'):
+        return False # Arquivo ainda não existe, então não tem nenhum CPF
+    
+    with open("clientes.csv", mode="r", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        next(leitor, None) # Pular o cabeçalho
+        for linha in leitor:
+            if linha[1] == cpf: # CPF é o segundo campo (indice 1)
+                return True
+    return False
 # =================================
 # Função responsável por cadastrar um novo cliente
 # =================================
 def cadastrar_clientes():
-    print("\n 📋 Cadastro de Cliente") # Título de seção de cadastro
+    while True:
+        limpar_tela()
+        cpf = input("CPF: ")
 
-    # Solicita as informações do cliente
-    nome = input("Nome completo: ")             # Nome do Cliente
-    cpf = input("CPF: ")                         #CPF do Cliente
-    telefone = input("Telefone: ")               # Telefone do Cliente
-    email = input("E-mail: ")                    # E-mail do Cliente
+        if cpf_ja_cadastrado(cpf):
+            limpar_tela()
+            print("\n⚠️ Usuário já cadastrado!")
+            time.sleep(3) # Espera 3 segundos
+            continue # Volta para o inicio do loop, pedindo CPF de novo
 
+        nome = input("Nome completo: ")
+        telefone = input("Telefone: ")
+        email = input("E-mail: ")
 
-    erros = verificar_dados(nome, cpf, telefone, email)
+        erros = verificar_dados(nome, cpf, telefone, email)
 
-    if erros:
-        print("\n❌ Forram encontrados os seguintes erros: ")
-        for erro in erros:
-            input("\n 🔁 Pressione Enter para tentar novamente....")
-    else:
-        # Abre (ou cria, se não existir) o arquivo clientes.csv no modo append (adicionar)
-        #newline='' evita linhas em branco extras no Windows
-        # encoding='utf-8' garante a leitura correta dos caracteres
+        if erros:
+            print("\n❌ Foram encontrados os seguintes erros:")
+            for erro in erros:
+                print(f"- {erro}")
+            input("\n 🔁 Pressione Enter para tentar novamente...")
+            return  # OK, está dentro da função
+
+        if cpf_ja_cadastrado(cpf):
+            print("\n⚠️ Este CPF já está cadastrado no sistema")
+            input("\n 🔁 Pressione Enter para tentar novamente...")
+            return  # OK, dentro da função
+
         arquivo_existe = os.path.isfile("clientes.csv")
-    with open("clientes.csv", mode="a", newline="", encoding="utf-8") as arquivo:
-        writer = csv.writer(arquivo)
-        if not arquivo_existe:
-            writer.writerow(["Nome", "CPF", "Telefone", "Email"])
-        writer.writerow([nome, cpf, telefone, email])
+        with open("clientes.csv", mode="a", newline="", encoding="utf-8") as arquivo:
+            writer = csv.writer(arquivo)
+            if not arquivo_existe:
+                writer.writerow(["Nome", "CPF", "Telefone", "Email"])
+            writer.writerow([nome, cpf, telefone, email])
 
-        # Mensagem de sucesso após o cadastro
         print("\n✅ Cliente cadastrado com sucesso!")
         input("\n🔙 Pressione Enter para voltar...")
 
@@ -69,7 +89,6 @@ def listar_clientes():
     limpar_tela()
     print("\n📂 Lista de Clientes:\n") # Título da seção de listagem
 
-
     # Verifica se o arquivo clientes.csv existe
     if not os.path.exists('clientes.csv'):
         print("Nenhum cliente cadastrado ainda.") # Mensagem caso o arquivo não exista
@@ -79,10 +98,19 @@ def listar_clientes():
     # Abre o arquivo em modo leitura
     with open('clientes.csv', mode='r', encoding='utf-8') as arquivo:
         leitor = csv.reader(arquivo) # Cria um objeto para ler o arquivo
+        next(leitor, None)# Ignora o Cabeçalho
+
+        contador = 0
         # Percorre todas as linhas do arquivo, exibindo cada cliente
         for i, linha in enumerate(leitor, start=1): # i é índice (começa em 1), linha é uma lista com os dados
+            if len(linha) != 4:
+                continue # Ignora linhas incompletas
             nome, cpf, telefone, email = linha #Desempacota os dados da linha
             print(f"{i}. Nome: {nome} | CPF: {cpf} | Telefone: {telefone} | E-mail: {email}") # Exibe os dados formatados
+            contador += 1
+
+            if contador == 0:
+                print("Nennhum cliente Válido encontrado.")
 
     input("\n 🔙 Pressione Enter para voltar ao menu...")
     
